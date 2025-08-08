33354d93a02a761d4fead66148ffecc296d1d821
KolfLDA <ducanhlv142@gmail.com>
2025-08-08 17:30:20 +0700

    Create UI App

--- main.js
+++ main.js
@@ -1,16 +1,35 @@
 const path = require('path');
 const {app, BrowserWindow} = require('electron');
 
+const isDev = process.env.NODE_ENV !== 'development';
+const isWin = process.platform === 'win64';
+
 function createMainWindow () {
     const mainWindow = new BrowserWindow({
         title: 'Image Resizer',
-        width: 700,
-        height: 600
+        width: isDev ? 1920 : 1000,
+        height: isDev ? 1080: 800
     });
+
+    //Open dev tools if in development mode
+    if (isDev) {
+        mainWindow.webContents.openDevTools();
+    }
 
     mainWindow.loadFile(path.join(__dirname, './renderer/index.html'));
 }
 
 app.whenReady().then(() => {
     createMainWindow();
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createMainWindow();
+        }
+    });
+});
+
+app.on('window-all-closed', () => {
+    if (!isWin) {
+        app.quit();
+    }
 });
